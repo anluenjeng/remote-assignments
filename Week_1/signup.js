216da05cd7c4c8822e755e1d867e5ce379674738
Ubuntu <ubuntu@ip-172-31-12-115.us-east-2.compute.internal>
2023-07-07 10:36:30 +0000

    Fianl?

--- Week_1/signup.js
+++ Week_1/signup.js
@@ -2,16 +2,31 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql2/promise');
 const bcrypt = require('bcrypt');
-const mysqlcon = require('./mysqlcon');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const app = express();
 const port = 3000;
+const secretKey = 'your-secret-key'; // 替換為你的密鑰
+
+function isValidEmail(email) {
+  // Regular expression to match email format
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  return emailRegex.test(email);
+}
 
 // 解析 JSON 格式的請求主體
 app.use(bodyParser.json());
 
 // 建立與 MySQL 的連線池
-const pool = mysql.createPool(mysqlcon);
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+});
 
 (async () => {
   try {
@@ -19,14 +34,19 @@ const pool = mysql.createPool(mysqlcon);
     console.log('Connected to MySQL');
 
     // 處理 POST 請求的路由
-    app.post('/users/signup', async (req, res) => {
+    app.post('/api/1.0/users/signup', async (req, res) => {
       try {
         // 取得請求中的資料
-        const { name, email, password } = req.body;
+        const { name, email, password } = req.body.data || req.body;
 
         // 檢查必填欄位是否存在
         if (!name || !email || !password) {
           res.status(400).json({ error: 'Missing fields' });
+          return;
+        }
+
+        if (!isValidEmail(email)) {
+          res.status(400).json({ error: 'Invalid email' });
           return;
         }
 
@@ -48,13 +68,24 @@ const pool = mysql.createPool(mysqlcon);
 
         const userResponse = {
           id: insertedUserId,
-          provider: 'facebook',
+          provider: 'native',
           name: name,
           email: email,
-          picture: 'https://schoolvoyage.ga/images/123498.png'
+          picture: 'url'
         };
 
-        res.status(200).json(userResponse);
+        // 生成 Access Token
+        const accessToken = jwt.sign(userResponse, secretKey);
+
+        // 回傳使用者資訊和 Access Token
+        const responseData = {
+          data: {
+            access_token: accessToken,
+            user: userResponse
+          }
+        };
+
+        res.status(200).json(responseData);
       } catch (error) {
         console.error('Error message:', error.message);
         res.status(500).json({ message: 'Server Error' });
@@ -69,4 +100,5 @@ const pool = mysql.createPool(mysqlcon);
     console.error('Error connecting to MySQL:', error);
   }
 })();
+
 
